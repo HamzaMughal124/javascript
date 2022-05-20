161be800d53b49a71f6de194012659b7287c3c91
Hamza Mughal <hm588940@gmail.com>
2022-05-20 10:24:44 -0700

    first commit

---
+++ 01.js
@@ -0,0 +1,201 @@
+//  Nested if else
+
+//  let winnigNum = 19;
+// let guessNum = Number(prompt("Guess a number "));
+
+// if( winnigNum ===  guessNum ){
+//     console.log("Your Guess is Right!!")
+// }else{
+//     if(guessNum > winnigNum){
+//         console.log("to High !!")
+//     }else{
+//         console.log("to low!!")
+//     }
+// }
+
+// if
+// else if
+// else
+
+// let deg = 40;
+
+// if(deg < 5){
+// console.log("to cold");
+// } else if(deg < 15){
+// console.log("normal");
+// }else if(deg < 25 ){
+//     console.log("go to swim");
+// }else if(deg < 35 ){
+//     console.log("turn on AC");
+// }else{
+//     console.log("To Warm")
+// }
+
+// switch case
+
+// let day = 8;
+
+// switch (day) {
+//   case 0:
+//     console.log("sunday");
+// //     break;
+//   case 1:
+//     console.log("monady");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wendsday");
+//     break;
+//   case 4:
+//     console.log("thursday");
+//     break;
+//   case 5:
+//     console.log("friday");
+//     break;
+//   case 6:
+//     console.log("saturday");
+//     break;
+//   default:
+//     console.log("invalid");
+// }
+
+// while loop
+
+// let i = 0;
+// let num = 10;
+// let total = 0;
+// while (i <= 10) {
+//   total = total + i;
+//   i++;
+// }
+// console.log(total);
+
+// for loop
+
+// for (let i = -10; i <= 0; i++) {
+//   console.log(i);
+// }
+
+// Array
+
+// let array = ["item1","item2",9]
+// console.log( array[2])
+
+// push
+
+// let array = ["item1","item2"]
+// array.push(4)
+// console.log(array)
+
+// pop
+
+// let array = ["item1","item2"]
+// array.pop()
+// console.log(array)
+
+// shift
+
+// let array = ["item1","item2"];
+// array.shift()
+// console.log(array)
+
+// unshift
+
+// let array = [1,2,3,4,5];
+// array.slice(1)
+// console.log(array)
+
+// clone array
+
+// let arr = [1, 2, 3, 4];
+// let arr1 = arr.slice(1);
+// console.log(arr1)
+
+// let arr = [1, 2, 3, 4];
+// let arr1 = arr.concat();
+
+// console.log(arr1)
+
+// spread oprater
+
+// let arr = [1,1,1,12];
+// let arr1 = [...arr]
+// console.log(arr1)
+
+// create array using const
+// const arr = [1,2,3,4];
+//  const arr2 = [11]
+// arr.push(33)
+// console.log(arr)
+// console.log(arr2)
+
+// while loop in array
+
+// let arr = ["apple", "mango", "banana", "grapes"];
+// console.log(arr)
+
+// let i = 0;
+// while (i < arr.length) {
+//   console.log(i);
+//   i++;
+// }
+
+// for of loop
+
+// let arr = [1,2,3,4];
+
+// for (let arr1 of arr){
+//   console.log(arr1)
+// }
+
+// for in loop
+
+// let arr = [1,2,3,4,3];
+// for (let arr1 in arr){
+//   console.log(arr[arr1])
+// }
+
+// array destructing
+
+// let arr = [1, 2, 3, 4];
+// console.log(arr);
+
+// let [arr1, arr2, ...arr3] = arr;
+
+// console.log(arr1);
+// console.log(arr2);
+// console.log(arr3);
+
+// intro to object
+// const key = "email";
+// const obj = {
+//   name: "hamza",
+//   age: 20,
+//   gender: "male",
+// };
+// obj[key] = "hamza@gmail.com"
+// console.log(obj);
+//  How to access data
+// console.log(obj.name); // dot notation
+// console.log(obj["age"]); // bracket notation
+
+// how to add key value pair
+// obj.hobbie = "gaming";
+
+//  How to iterate object
+
+// const person = {
+//   name: "hamza",
+//   age: 20,
+//   gender: "male",
+// };
+// for(let key in person ){
+//     // console.log(`${obj1} : ${obj[obj1]}`);
+//     // console.log(key," : ",person[key]);
+//     // console.log(obj[obj1]);
+// }
+
+//  Computed properties
+
